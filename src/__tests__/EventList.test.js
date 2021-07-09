1bba7587252f111104f6e6c3708531d061d24366
TerryL1971 <tcl71771@yahoo.com>
2021-07-09 22:24:46 +0200

    Update EventList.test.js
    added mockData.length to toHavelength()

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -4,10 +4,9 @@ import EventList from '../EventList';
 import Event from '../Event';
 import { mockData } from '../mock-data';
 
-
 describe('<EventList /> component', () => {
   test('render correct number of events', () => {
-    const EventListWrapper = shallow(<EventList events={mockData} />);
-    expect(EventListWrapper.find(Event)).toHaveLength(10);
+  const EventListWrapper = shallow(<EventList events={mockData} />);
+  expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
   });
 });
