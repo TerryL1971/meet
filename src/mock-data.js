02cac93c645668036693085cad71b2fbaa97ab33
TerryL1971 <tcl71771@yahoo.com>
2021-07-14 18:23:50 +0200

    Update mock-data.js

--- src/mock-data.js
+++ src/mock-data.js
@@ -1,6 +1,76 @@
 let mockData = [
   {
    "kind": "calendar#event",
+   "etag": "\"3187221858184000\"",
+   "id": "3gi3kabkm3bua5lq3jhscc8s4c_20200810T200000Z",
+   "status": "confirmed",
+   "htmlLink": "https://www.google.com/calendar/event?eid=M2dpM2thYmttM2J1YTVscTNqaHNjYzhzNGNfMjAyMDA4MTBUMjAwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+   "created": "2020-07-01T13:23:24.000Z",
+   "updated": "2020-07-01T13:42:09.092Z",
+   "summary": "Intro to AngularJS-Remote",
+   "description": "Developed by Google, AngularJS is a relatively new JavaScript, and it is designed to make front-end development as easy as possible for you. Join us to get introduced to this wonderful framework and dive deep into its features. ",
+   "location": "New York, NY, USA",
+   "creator": {
+    "email": "fullstackwebdev@careerfoundry.com",
+    "self": true
+   },
+   "organizer": {
+    "email": "fullstackwebdev@careerfoundry.com",
+    "self": true
+   },
+   "start": {
+    "dateTime": "2020-08-10T22:00:00+02:00",
+    "timeZone": "Europe/Berlin"
+   },
+   "end": {
+    "dateTime": "2020-08-10T23:00:00+02:00",
+    "timeZone": "Europe/Berlin"
+   },
+   "recurringEventId": "3gi3kabkm3bua5lq3jhscc8s4c",
+   "originalStartTime": {
+    "dateTime": "2020-08-10T22:00:00+02:00",
+    "timeZone": "Europe/Berlin"
+   },
+   "iCalUID": "3gi3kabkm3bua5lq3jhscc8s4c@google.com",
+   "sequence": 2,
+   "hangoutLink": "https://meet.google.com/xcb-hfjb-psv",
+   "conferenceData": {
+    "entryPoints": [
+     {
+      "entryPointType": "video",
+      "uri": "https://meet.google.com/xcb-hfjb-psv",
+      "label": "meet.google.com/xcb-hfjb-psv"
+     },
+     {
+      "entryPointType": "more",
+      "uri": "https://tel.meet/xcb-hfjb-psv?pin=4442155852117",
+      "pin": "4442155852117"
+     },
+     {
+      "regionCode": "DE",
+      "entryPointType": "phone",
+      "uri": "tel:+49-40-8081617300",
+      "label": "+49 40 8081617300",
+      "pin": "657955248"
+     }
+    ],
+    "conferenceSolution": {
+     "key": {
+      "type": "hangoutsMeet"
+     },
+     "name": "Google Meet",
+     "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+    },
+    "conferenceId": "xcb-hfjb-psv",
+    "signature": "ACn9hYHvjRGBCkZ5zIbnqK+zQYpH"
+   },
+   "reminders": {
+    "useDefault": true
+   },
+   "eventType": "default"
+  },
+  {
+   "kind": "calendar#event",
    "etag": "\"3181161784712000\"",
    "id": "4eahs9ghkhrvkld72hogu9ph3e_20200519T140000Z",
    "status": "confirmed",
@@ -72,6 +142,146 @@ let mockData = [
    },
    "iCalUID": "3qtd6uscq4tsi6gc7nmmtpqlct@google.com",
    "sequence": 0,
+   "reminders": {
+    "useDefault": true
+   },
+   "eventType": "default"
+  },
+  {
+   "kind": "calendar#event",
+   "etag": "\"3187221506816000\"",
+   "id": "48m09le389vijbqfs2mcr6i87q_20200811T070000Z",
+   "status": "confirmed",
+   "htmlLink": "https://www.google.com/calendar/event?eid=NDhtMDlsZTM4OXZpamJxZnMybWNyNmk4N3FfMjAyMDA4MTFUMDcwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+   "created": "2020-07-01T13:39:01.000Z",
+   "updated": "2020-07-01T13:39:13.408Z",
+   "summary": "Node Gang",
+   "description": "We meet every Tuesday to talk about Node or JavaScript in general. Node Gang is an inclusive community that tries to cater to all levels of learners during meetups. Join us if you are curious to hear about what's new in Node, patterns, interesting npm packages, and practices.",
+   "location": "Sydney NSW, Australia",
+   "creator": {
+    "email": "fullstackwebdev@careerfoundry.com",
+    "self": true
+   },
+   "organizer": {
+    "email": "fullstackwebdev@careerfoundry.com",
+    "self": true
+   },
+   "start": {
+    "dateTime": "2020-08-11T09:00:00+02:00",
+    "timeZone": "Europe/Berlin"
+   },
+   "end": {
+    "dateTime": "2020-08-11T10:00:00+02:00",
+    "timeZone": "Europe/Berlin"
+   },
+   "recurringEventId": "48m09le389vijbqfs2mcr6i87q",
+   "originalStartTime": {
+    "dateTime": "2020-08-11T09:00:00+02:00",
+    "timeZone": "Europe/Berlin"
+   },
+   "iCalUID": "48m09le389vijbqfs2mcr6i87q@google.com",
+   "sequence": 1,
+   "hangoutLink": "https://meet.google.com/wdc-wutt-tji",
+   "conferenceData": {
+    "entryPoints": [
+     {
+      "entryPointType": "video",
+      "uri": "https://meet.google.com/wdc-wutt-tji",
+      "label": "meet.google.com/wdc-wutt-tji"
+     },
+     {
+      "entryPointType": "more",
+      "uri": "https://tel.meet/wdc-wutt-tji?pin=3796108774207",
+      "pin": "3796108774207"
+     },
+     {
+      "regionCode": "DE",
+      "entryPointType": "phone",
+      "uri": "tel:+49-40-8081618974",
+      "label": "+49 40 8081618974",
+      "pin": "170634768"
+     }
+    ],
+    "conferenceSolution": {
+     "key": {
+      "type": "hangoutsMeet"
+     },
+     "name": "Google Meet",
+     "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+    },
+    "conferenceId": "wdc-wutt-tji",
+    "signature": "ACn9hYEEmgGHK9aIf7jkIzc8hijj"
+   },
+   "reminders": {
+    "useDefault": true
+   },
+   "eventType": "default"
+  },
+  {
+   "kind": "calendar#event",
+   "etag": "\"3187232532588000\"",
+   "id": "20c51je3q99mnolr1rp06dt9jd_20200811T120000Z",
+   "status": "confirmed",
+   "htmlLink": "https://www.google.com/calendar/event?eid=MjBjNTFqZTNxOTltbm9scjFycDA2ZHQ5amRfMjAyMDA4MTFUMTIwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+   "created": "2020-07-01T14:17:12.000Z",
+   "updated": "2020-07-01T15:11:06.294Z",
+   "summary": "Use jQuery, bring in interactivity easily ",
+   "description": "Do you know jQuery is used by around 70 percent of the 10 million most popular websites as of May 2019? \n\nThough many consider it dead after Angular and Express gained popularity, jQuery is still an important part of many websites. \n\nIn our workshop, we teach basic to advanced jQuery where you will also be able to build a simple app using it. If you are familiar with JS, join us to learn probably its most popular library.",
+   "location": "Mumbai, Maharashtra, India",
+   "creator": {
+    "email": "fullstackwebdev@careerfoundry.com",
+    "self": true
+   },
+   "organizer": {
+    "email": "fullstackwebdev@careerfoundry.com",
+    "self": true
+   },
+   "start": {
+    "dateTime": "2020-08-11T14:00:00+02:00",
+    "timeZone": "Europe/Berlin"
+   },
+   "end": {
+    "dateTime": "2020-08-11T15:00:00+02:00",
+    "timeZone": "Europe/Berlin"
+   },
+   "recurringEventId": "20c51je3q99mnolr1rp06dt9jd",
+   "originalStartTime": {
+    "dateTime": "2020-08-11T14:00:00+02:00",
+    "timeZone": "Europe/Berlin"
+   },
+   "iCalUID": "20c51je3q99mnolr1rp06dt9jd@google.com",
+   "sequence": 1,
+   "hangoutLink": "https://meet.google.com/nns-htdr-ijk",
+   "conferenceData": {
+    "entryPoints": [
+     {
+      "entryPointType": "video",
+      "uri": "https://meet.google.com/nns-htdr-ijk",
+      "label": "meet.google.com/nns-htdr-ijk"
+     },
+     {
+      "entryPointType": "more",
+      "uri": "https://tel.meet/nns-htdr-ijk?pin=9302146320475",
+      "pin": "9302146320475"
+     },
+     {
+      "regionCode": "DE",
+      "entryPointType": "phone",
+      "uri": "tel:+49-40-8081618430",
+      "label": "+49 40 8081618430",
+      "pin": "810644618"
+     }
+    ],
+    "conferenceSolution": {
+     "key": {
+      "type": "hangoutsMeet"
+     },
+     "name": "Google Meet",
+     "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+    },
+    "conferenceId": "nns-htdr-ijk",
+    "signature": "ACn9hYGc8p6xxbn/zOCh7jsX4wgC"
+   },
    "reminders": {
     "useDefault": true
    },
@@ -194,201 +404,7 @@ let mockData = [
    },
    "eventType": "default"
   },
-  {
-   "kind": "calendar#event",
-   "etag": "\"3181159875584000\"",
-   "id": "3qtd6uscq4tsi6gc7nmmtpqlct_20200525T120000Z",
-   "status": "confirmed",
-   "htmlLink": "https://www.google.com/calendar/event?eid=M3F0ZDZ1c2NxNHRzaTZnYzdubW10cHFsY3RfMjAyMDA1MjVUMTIwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
-   "created": "2020-05-19T19:14:30.000Z",
-   "updated": "2020-05-27T11:45:37.792Z",
-   "summary": "React is Fun",
-   "description": "Love HTML, CSS, and JS? Want to become a cool front-end developer? \n\nReact is one of the most popular front-end frameworks. There is a huge number of job openings for React developers in most cities. \n\nJoin us in our free React training sessions and give your career a new direction. ",
-   "location": "Berlin, Germany",
-   "creator": {
-    "email": "fullstackwebdev@careerfoundry.com",
-    "self": true
-   },
-   "organizer": {
-    "email": "fullstackwebdev@careerfoundry.com",
-    "self": true
-   },
-   "start": {
-    "dateTime": "2020-05-25T14:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "end": {
-    "dateTime": "2020-05-25T15:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "recurringEventId": "3qtd6uscq4tsi6gc7nmmtpqlct",
-   "originalStartTime": {
-    "dateTime": "2020-05-25T14:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "iCalUID": "3qtd6uscq4tsi6gc7nmmtpqlct@google.com",
-   "sequence": 0,
-   "reminders": {
-    "useDefault": true
-   },
-   "eventType": "default"
-  },
-  {
-   "kind": "calendar#event",
-   "etag": "\"3181161784712000\"",
-   "id": "4eahs9ghkhrvkld72hogu9ph3e_20200526T140000Z",
-   "status": "confirmed",
-   "htmlLink": "https://www.google.com/calendar/event?eid=NGVhaHM5Z2hraHJ2a2xkNzJob2d1OXBoM2VfMjAyMDA1MjZUMTQwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
-   "created": "2020-05-19T19:17:46.000Z",
-   "updated": "2020-05-27T12:01:32.356Z",
-   "summary": "Learn JavaScript",
-   "description": "Have you wondered how you can ask Google to show you the list of the top ten must-see places in London? And how Google presents you the list? How can you submit the details of an application? Well, JavaScript is doing these. :) \n\nJavascript offers interactivity to a dull, static website. Come, learn JavaScript with us and make those beautiful websites.",
-   "location": "London, UK",
-   "creator": {
-    "email": "fullstackwebdev@careerfoundry.com",
-    "self": true
-   },
-   "organizer": {
-    "email": "fullstackwebdev@careerfoundry.com",
-    "self": true
-   },
-   "start": {
-    "dateTime": "2020-05-26T16:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "end": {
-    "dateTime": "2020-05-26T17:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "recurringEventId": "4eahs9ghkhrvkld72hogu9ph3e",
-   "originalStartTime": {
-    "dateTime": "2020-05-26T16:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "iCalUID": "4eahs9ghkhrvkld72hogu9ph3e@google.com",
-   "sequence": 0,
-   "reminders": {
-    "useDefault": true
-   },
-   "eventType": "default"
-  },
-  {
-   "kind": "calendar#event",
-   "etag": "\"3181159875584000\"",
-   "id": "3qtd6uscq4tsi6gc7nmmtpqlct_20200527T120000Z",
-   "status": "confirmed",
-   "htmlLink": "https://www.google.com/calendar/event?eid=M3F0ZDZ1c2NxNHRzaTZnYzdubW10cHFsY3RfMjAyMDA1MjdUMTIwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
-   "created": "2020-05-19T19:14:30.000Z",
-   "updated": "2020-05-27T11:45:37.792Z",
-   "summary": "React is Fun",
-   "description": "Love HTML, CSS, and JS? Want to become a cool front-end developer? \n\nReact is one of the most popular front-end frameworks. There is a huge number of job openings for React developers in most cities. \n\nJoin us in our free React training sessions and give your career a new direction. ",
-   "location": "Berlin, Germany",
-   "creator": {
-    "email": "fullstackwebdev@careerfoundry.com",
-    "self": true
-   },
-   "organizer": {
-    "email": "fullstackwebdev@careerfoundry.com",
-    "self": true
-   },
-   "start": {
-    "dateTime": "2020-05-27T14:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "end": {
-    "dateTime": "2020-05-27T15:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "recurringEventId": "3qtd6uscq4tsi6gc7nmmtpqlct",
-   "originalStartTime": {
-    "dateTime": "2020-05-27T14:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "iCalUID": "3qtd6uscq4tsi6gc7nmmtpqlct@google.com",
-   "sequence": 0,
-   "reminders": {
-    "useDefault": true
-   },
-   "eventType": "default"
-  },
-  {
-   "kind": "calendar#event",
-   "etag": "\"3181161784712000\"",
-   "id": "4eahs9ghkhrvkld72hogu9ph3e_20200528T140000Z",
-   "status": "confirmed",
-   "htmlLink": "https://www.google.com/calendar/event?eid=NGVhaHM5Z2hraHJ2a2xkNzJob2d1OXBoM2VfMjAyMDA1MjhUMTQwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
-   "created": "2020-05-19T19:17:46.000Z",
-   "updated": "2020-05-27T12:01:32.356Z",
-   "summary": "Learn JavaScript",
-   "description": "Have you wondered how you can ask Google to show you the list of the top ten must-see places in London? And how Google presents you the list? How can you submit the details of an application? Well, JavaScript is doing these. :) \n\nJavascript offers interactivity to a dull, static website. Come, learn JavaScript with us and make those beautiful websites.",
-   "location": "London, UK",
-   "creator": {
-    "email": "fullstackwebdev@careerfoundry.com",
-    "self": true
-   },
-   "organizer": {
-    "email": "fullstackwebdev@careerfoundry.com",
-    "self": true
-   },
-   "start": {
-    "dateTime": "2020-05-28T16:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "end": {
-    "dateTime": "2020-05-28T17:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "recurringEventId": "4eahs9ghkhrvkld72hogu9ph3e",
-   "originalStartTime": {
-    "dateTime": "2020-05-28T16:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "iCalUID": "4eahs9ghkhrvkld72hogu9ph3e@google.com",
-   "sequence": 0,
-   "reminders": {
-    "useDefault": true
-   },
-   "eventType": "default"
-  },
-  {
-   "kind": "calendar#event",
-   "etag": "\"3181159875584000\"",
-   "id": "3qtd6uscq4tsi6gc7nmmtpqlct_20200529T120000Z",
-   "status": "confirmed",
-   "htmlLink": "https://www.google.com/calendar/event?eid=M3F0ZDZ1c2NxNHRzaTZnYzdubW10cHFsY3RfMjAyMDA1MjlUMTIwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
-   "created": "2020-05-19T19:14:30.000Z",
-   "updated": "2020-05-27T11:45:37.792Z",
-   "summary": "React is Fun",
-   "description": "Love HTML, CSS, and JS? Want to become a cool front-end developer? \n\nReact is one of the most popular front-end frameworks. There is a huge number of job openings for React developers in most cities. \n\nJoin us in our free React training sessions and give your career a new direction. ",
-   "location": "Berlin, Germany",
-   "creator": {
-    "email": "fullstackwebdev@careerfoundry.com",
-    "self": true
-   },
-   "organizer": {
-    "email": "fullstackwebdev@careerfoundry.com",
-    "self": true
-   },
-   "start": {
-    "dateTime": "2020-05-29T14:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "end": {
-    "dateTime": "2020-05-29T15:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "recurringEventId": "3qtd6uscq4tsi6gc7nmmtpqlct",
-   "originalStartTime": {
-    "dateTime": "2020-05-29T14:00:00+02:00",
-    "timeZone": "Europe/Berlin"
-   },
-   "iCalUID": "3qtd6uscq4tsi6gc7nmmtpqlct@google.com",
-   "sequence": 0,
-   "reminders": {
-    "useDefault": true
-   },
-   "eventType": "default"
-  },
+  
  ];
 
 mockData = JSON.parse(JSON.stringify(mockData));
