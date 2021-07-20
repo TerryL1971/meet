2fa2a20f295c5ffb9d9fcef92a99ea34c50f2b7d
TerryL1971 <tcl71771@yahoo.com>
2021-07-20 11:33:05 +0200

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
 
-
-
 class NumberOfEvents extends Component {
-	render() {
-		return (
-			<div className='NumberOfEvents'>
-			<label>Number of Events: </label>
-				<input
-					id='event-number'
-					type='number'
-					defaultValue={this.props.number}
-					onChange={(e) => this.props.updateNumber(e.target.value)}></input>
-			</div>
-		);
-	}
+  state = {
+    numberDisplayed: 32,
+  }
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.props.updateEvents(null, value);
+
+    if (value < 1) {
+      return this.setState({
+        numberDisplayed: '',
+        errorText: 'Please select a number between 1 and 32',
+      });
+    } else if (value > 32) {
+      return this.setState({
+        numberDisplayed: '',
+        errorText: 'Please select a number between 1 and 32',
+      });
+    } else {
+      this.setState({
+        numberDisplayed: value,
+        errorText: '',
+      });
+    }
+  };
+
+
+  render() {
+    const numberDisplayed = this.state.numberDisplayed;
+    return (
+
+      <div className="NumberOfEvents ">
+
+        <div className="py-3 d-flex flex-row-reverse align-items-center">
+					<span>Number of Events: </span>
+          <input
+            type="number"
+            className="event-number-input rounded text-align-center"
+            value={numberDisplayed}
+            onChange={this.handleInputChanged} />
+        </div>
+
+        
+      </div>
+
+    );
+  }
 }
 
 export default NumberOfEvents;
